f777bffc198058195608b3cb68c9dbf29c157c1a
Nikolai Angelov <nikolaiangelovnix@gmail.com>
2023-07-14 12:48:04 +0300

    more up

--- iterators_and_generators/reverse_string.py
+++ iterators_and_generators/reverse_string.py
@@ -1,2 +1,7 @@
 def reverse_text(text):
-    
+    current_index = len(text) - 1
+    end_index = 0
+    while current_index >= end_index:
+        yield text[current_index]
+        current_index -= 1
+        
